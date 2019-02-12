d2283c54dd98efdd71173f7218518b69fc3799af
evan_malkin <evan_malkin@student.wayland.k12.ma.us>
2019-02-12 08:31:52 -0500

    hey guys whats up 2

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,77 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/homework', function(req, res){
 
+  // Get a Mongo client to work with the Mongo server
+  var MongoClient = mongodb.MongoClient;
+
+  // Define where the MongoDB server is
+  var url = 'mongodb://localhost:27017/local';
+
+  // Connect to the server
+  MongoClient.connect(url, function (err, db) {
+    if (err) {
+      console.log('Unable to connect to the Server', err);
+    } else {
+      // We are connected
+      console.log('Connection established to', url);
+
+      // Get the documents collection
+      var collection = db.collection('homework');
+
+      // Find all students
+      collection.find({}).toArray(function (err, result) {
+        if (err) {
+          res.send(err);
+        } else if (result.length) {
+          res.render('homework',{
+
+            // Pass the returned database documents to Jade
+            "homework" : result
+          });
+        } else {
+          res.send('No documents found');
+        }
+        //Close connection
+        db.close();
+      });
+    }
+  });
+});
+
+router.get('/newhomework', function (req, res) {
+  res.render('newhomework', {title: 'add homework'});
+});
+
+router.post('/addhomework', function (req, res) {
+  var MongoClient = mongodb.MongoClient;
+
+  var url = 'mongodb://localhost:27017/local';
+
+  MongoClient.connect(url, function (err, db) {
+    if(err){
+      console.log('unable to connect to server', err);
+    }else{
+      console.log('connected to server');
+
+      var collection = db.collection('homework');
+
+      var student1 = {student: req.body.student, street: req.body.street, city: req.body.city, state: req.body.state, sex: req.body.sex, gpa: req.body.gpa};
+
+      collection.insert([student1], function (err, result) {
+        if(err){
+          console.log('unable to connect', err);
+        }else{
+          res.redirect('/homework');
+        }
+        db.close();
+      });
+    }
+
+
+  })
+});
 
 module.exports = router;
 
