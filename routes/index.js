71d54b3b81949eaa9307ccc99efc04b858b2584c
timothy_kwan <timothy_kwan@student.wayland.k12.ma.us>
2019-03-13 08:22:05 -0400

    more deleted stuff

--- routes/index.js
+++ routes/index.js
@@ -6,44 +6,46 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Homework' });
 });
 
-router.get('/homework', function(req, res){
 
-  // Get a Mongo client to work with the Mongo server
-  var MongoClient = mongodb.MongoClient;
 
-  // Define where the MongoDB server is
-  var url = 'mongodb://localhost:3001/local';
-
-  // Connect to the server
-  MongoClient.connect(url, function (err, db) {
-    if (err) {
-      console.log('Unable to connect to the Server', err);
-    } else {
-      // We are connected
-      console.log('Connection established to', url);
-
-      // Get the documents collection
-      var collection = db.collection('users');
-
-      // Find all students
-      collection.find({}).toArray(function (err, result) {
-        if (err) {
-          res.send(err);
-        } else if (result.length) {
-          res.render('homework',{
-
-            // Pass the returned database documents to Jade
-            "homework" : result
-          });
-        } else {
-          res.send('No documents found');
-        }
-        //Close connection
-        db.close();
-      });
-    }
-  });
-});
+// router.get('/homework', function(req, res){
+//
+//   // Get a Mongo client to work with the Mongo server
+//   var MongoClient = mongodb.MongoClient;
+//
+//   // Define where the MongoDB server is
+//   var url = 'mongodb://localhost:3000/local';
+//
+//   // Connect to the server
+//   MongoClient.connect(url, function (err, db) {
+//     if (err) {
+//       console.log('Unable to connect to the Server', err);
+//     } else {
+//       // We are connected
+//       console.log('Connection established to', url);
+//
+//       // Get the documents collection
+//       var collection = db.collection('users');
+//
+//       // Find all students
+//       collection.find({}).toArray(function (err, result) {
+//         if (err) {
+//           res.send(err);
+//         } else if (result.length) {
+//           res.render('homework',{
+//
+//             // Pass the returned database documents to Jade
+//             "homework" : result
+//           });
+//         } else {
+//           res.send('No documents found');
+//         }
+//         //Close connection
+//         db.close();
+//       });
+//     }
+//   });
+// });
 
 module.exports = router;
 
